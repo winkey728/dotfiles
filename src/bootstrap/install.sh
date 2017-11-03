fc4c90088d6dfbd3d5e49ef7e96cabde5091a6e5
winkey <winkeyme@163.com>
2017-11-03 11:56:39 +0800

    Initial the files of installations and preferences

--- src/bootstrap/install.sh
+++ src/bootstrap/install.sh
@@ -1,1 +1,20 @@
 #!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "utils.sh"
+
+# ----------------------------------------------------------------------
+# | Main                                                               |
+# ----------------------------------------------------------------------
+
+main() {
+
+    declare -r FILE_PATH="../installations"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    print_in_purple "\n • Installations\n\n"
+
+    "$FILE_PATH/$(get_os)/main.sh"
+
+}
--- src/bootstrap/preferences.sh
+++ src/bootstrap/preferences.sh
@@ -1,1 +1,20 @@
 #!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "utils.sh"
+
+# ----------------------------------------------------------------------
+# | Main                                                               |
+# ----------------------------------------------------------------------
+
+main() {
+
+    declare -r FILE_PATH="../preferences"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    print_in_purple "\n • Preferences\n\n"
+
+    "$FILE_PATH/$(get_os)/main.sh"
+
+}
