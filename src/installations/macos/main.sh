0e344df04774bb1519d3cee497c9815e356d259c
winkey <winkeyme@163.com>
2017-11-03 15:42:12 +0800

    Add macos basic setups
    I define some installation script files which do not create yet. I will create
    them later.

---
+++ src/installations/macos/main.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../bootstrap/utils.sh" \
+    && . "utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    ./xcode.sh
+    ./homebrew.sh
+
+    ../zsh.sh
+    ../git.sh
+    ../command_line_tools.sh
+    ../java.sh
+    ../rust.sh
+    ../python.sh
+    ../nodejs.sh
+    ../shell-script.sh
+    ../uml.sh
+    ../misc.sh
+
+    ./emacs.sh
+    ./browser.sh
+    ./terminal.sh
+    ./mail.sh
+    ./gfw.sh
+    ./media.sh
+    ./chat.sh
+    ./design.sh
+    ./netdisk.sh
+    ./misc_app.sh
+
+    ./cleanup.sh
+
+}
+
+main "$@"
