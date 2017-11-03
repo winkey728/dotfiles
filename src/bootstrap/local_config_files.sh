594eca1899ad73ad3b95bf2cb8e19ce7e7d4d2c3
winkey <winkeyme@163.com>
2017-11-03 11:38:32 +0800

    Create local config files

--- src/bootstrap/local_config_files.sh
+++ src/bootstrap/local_config_files.sh
@@ -1,1 +1,101 @@
 #!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "utils.sh"
+
+# ----------------------------------------------------------------------
+# | Helper functions                                                   |
+# ----------------------------------------------------------------------
+
+create_zsh_local() {
+
+    declare -r FILE_PATH="$HOME/.zsh.local"
+
+    if [ ! -e "$FILE_PATH" ] || [ -z "$FILE_PATH" ]; then
+
+        printf "" >> "$FILE_PATH"
+
+    fi
+
+    print_result $? "$FILE_PATH"
+
+}
+
+create_gitconfig_local() {
+
+    declare -r FILE_PATH="$HOME/.gitconfig.local"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    local gitUser=""
+    local gitEmail=""
+    local skipQuestions=false
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if [ ! -e "$FILE_PATH" ] || [ -z "$FILE_PATH" ]; then
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        skip_questions "$@" && skipQuestions=true
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        if ! $skipQuestions; then
+
+            ask "Please provide your git username: "
+            gitUser="$(get_answer)"
+
+            ask "Please provide your git email: "
+            gitEmail="$(get_answer)"
+
+        fi
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        printf "%s\n" \
+"[commit]
+
+    # Sign commits using GPG.
+    # https://help.github.com/articles/signing-commits-using-gpg/
+
+    # gpgsign = true
+
+[diff]
+
+    # You can use any diff tool here(e.g diffmerge)
+
+    # tool = diffmerge
+
+[merge]
+
+    # You can use any merge tool here(e.g diffmerge)
+
+    # tool = diffmerge
+
+[user]
+
+    name = $gitUser
+    email = $gitEmail
+    # signingkey =" \
+        >> "$FILE_PATH"
+   fi
+
+    print_result $? "$FILE_PATH"
+
+}
+
+# ----------------------------------------------------------------------
+# | Main                                                               |
+# ----------------------------------------------------------------------
+
+main() {
+
+    print_in_purple "\n • Create local config files\n\n"
+
+
+    create_gitconfig_local "$@"
+
+}
+
+main "$@"
