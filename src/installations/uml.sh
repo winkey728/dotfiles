6860d52427eb84aa8d5e5d25876f88d1c8dcc8eb
winkey <winkeyme@163.com>
2017-11-08 11:06:58 +0800

    Add shellcheck and plantuml installation

---
+++ src/installations/uml.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../bootstrap/utils.sh" \
+    && . "utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_in_purple "\n   UML\n\n"
+
+    brew_install "PlantUML" "plantuml"
+
+}
+
+main "$@"
