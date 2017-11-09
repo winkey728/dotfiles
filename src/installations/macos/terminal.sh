1556d1939c7ead6c0bf9072014113b41e5030c12
winkey <winkeyme@163.com>
2017-11-09 15:04:02 +0800

    Add terminal installations

---
+++ src/installations/macos/terminal.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../bootstrap/utils.sh" \
+    && . "../utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_in_purple "\n   Terminals\n\n"
+
+    brew_install "iTerm2" "iterm2"
+
+}
