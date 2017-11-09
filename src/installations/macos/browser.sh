ba3af134aee062f832ba81800fc3b88ccb058e6c
winkey <winkeyme@163.com>
2017-11-09 14:35:01 +0800

    Add browsers installation

---
+++ src/installations/macos/browser.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../bootstrap/utils.sh" \
+    && . "../utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_in_purple "\n   Browsers\n\n"
+
+    brew_install "Chrome" "google-chrome" "caskroom/cask" "cask"
+
+}
