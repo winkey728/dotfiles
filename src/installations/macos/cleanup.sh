6c65cbdc71f3162ec33b8ef368b3491bad5790ac
winkey <winkeyme@163.com>
2017-11-09 16:46:02 +0800

    Add cleanup script

---
+++ src/installations/macos/cleanup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_in_purple "\n   Cleanup\n\n"
+
+    brew_cleanup
+
+}
+
+main "$@"
