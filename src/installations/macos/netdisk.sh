2a449809f7169feec18e50959f1096b496c41343
winkey <winkeyme@163.com>
2017-11-16 18:12:12 +0800

    Remove Nutstore installation
    The Nutstore can only be installed manully

--- src/installations/macos/netdisk.sh
+++ src/installations/macos/netdisk.sh
@@ -11,7 +11,16 @@ main() {
     print_in_purple "\n   Network Disk Applications\n\n"
 
     brew_install "Dropbox" "dropbox" "caskroom/cask" "cask"
-    brew_install "Nutstore" "nutstore" "caskroom/cask" "cask"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Nutstore can only be installed manually, see
+    # https://github.com/caskroom/homebrew-cask/blob/master/Casks/nutstore.rb
+
+    # brew_install "Nutstore" "nutstore" "caskroom/cask" "cask"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
     brew_install "WizNote" "wiznote" "caskroom/cask" "cask"
     brew_install "Enpass" "enpass" "caskroom/cask" "cask"
 
