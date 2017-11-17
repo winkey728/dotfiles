9ffdc2505d68d4bfc885dd80f66572ebb76f740f
winkey <winkeyme@163.com>
2017-11-17 09:56:33 +0800

    Make two script executable and fix iterm2 preferences error

--- src/preferences/macos/terminal.sh
+++ src/preferences/macos/terminal.sh
@@ -10,8 +10,11 @@ main() {
     print_in_purple "\n   Terminal\n\n"
 
     execute \
-        "defaults write com.googlecode.iterm2 PrefsCustomFolder -string '$HOME/.iTerm/' \
-         defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true" \
+        "defaults write com.googlecode.iterm2 PrefsCustomFolder -string '$HOME/.iTerm/'" \
+        "Set preferences custom folder for iTerm"
+
+    execute \
+        "defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true" \
         "Load preferences from custom folder for iTerm"
 
     killall "iTerm" &> /dev/null
