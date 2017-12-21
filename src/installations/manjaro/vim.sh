3d1fd8c25fa567257afcda4e65952d2548b85a14
winkey <winkeyme@163.com>
2017-12-21 23:13:44 +0800

    [Manjaro -> vim] Install

---
+++ src/installations/manjaro/vim.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../bootstrap/utils.sh" \
+    && . "utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    pacman_install "Vim" "vim"
+
+}
+
+main "$@"
