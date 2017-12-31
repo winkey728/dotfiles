4513a5ae6a19548c3413fe5c785e593fc058a944
winkey <winkeyme@163.com>
2017-12-31 22:55:29 +0800

    [Manjaro -> gfw] Install and configure shadowsocks-qt5 and proxychains-ng
    Whilst fix detecting issue of proxychains configuration on macOS

--- src/installations/manjaro/gfw.sh
+++ src/installations/manjaro/gfw.sh
@@ -6,10 +6,10 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-declare -r PROXYCHAINS_CONFIG_FILE="/usr/local/etc/proxychains.conf"
+declare -r PROXYCHAINS_CONFIG_FILE="/etc/proxychains.conf"
 
 declare -r DEFAULT_SOCKS5_HOST="127.0.0.1"
-declare -r DEFAULT_SOCKS5_PORT="1086"
+declare -r DEFAULT_SOCKS5_PORT="1080"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -30,7 +30,7 @@ socks5 $DEFAULT_SOCKS5_HOST $DEFAULT_SOCKS5_PORT
     if ! file_contains_string "^socks5 $DEFAULT_SOCKS5_HOST $DEFAULT_SOCKS5_PORT" "$PROXYCHAINS_CONFIG_FILE"; then
 
         execute \
-            "printf '%s' '$CONFIG' >> $PROXYCHAINS_CONFIG_FILE" \
+            "printf '%s' '$CONFIG' | sudo tee -a $PROXYCHAINS_CONFIG_FILE" \
             "proxychains-ng (update config)"
 
     fi
@@ -43,9 +43,9 @@ main() {
 
     print_in_purple "\n   GFW Applications\n\n"
 
-    brew_install "ShadowsocksX-NG" "shadowsocksx-ng" "caskroom/cask" "cask"
+    pacman_install "Shadowsocks-qt5" "shadowsocks-qt5"
 
-    brew_install "proxychains-ng" "proxychains-ng" \
+    pacman_install "proxychains-ng" "proxychains-ng" \
         && set_proxychains_config
 
 }
