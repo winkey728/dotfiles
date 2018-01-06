44bc0386545fb1397103ec9b6d679c552a88ff4e
winkey <winkeyme@163.com>
2018-01-07 00:22:12 +0800

    [Mac -> design] Remove Google Web Designer

--- src/installations/macos/design.sh
+++ src/installations/macos/design.sh
@@ -12,7 +12,6 @@ main() {
 
     # brew_install "Sketch APP" "sketch" "caskroom/cask" "cask"
     brew_install "Sketchpacks" "sketchpacks" "caskroom/cask" "cask"
-    brew_install "Google Web Designer" "google-web-designer" "caskroom/cask" "cask"
 
 }
 
