6a46703613d458fadb4063a50ddbe2a1ec262621
winkey <winkeyme@163.com>
2018-01-17 09:50:06 +0800

    [Mac -> zsh] Reable path helper and setup environment in zprofile
    No need to disable path helper, which will overwrite $PATH in /etc/zprofile,
    because I set $PATH in $HOME/.zprofile now.

--- src/installations/macos/zsh.sh
+++ src/installations/macos/zsh.sh
@@ -46,42 +46,12 @@ change_default_shell() {
 
 }
 
-disable_path_helper() {
-
-    # Remove the path of the macOS added through `path_helper`, which
-    # those path are defined in `/etc/paths`.
-    #
-    # This needs to be done because some applications use their own
-    # PATH to run but the system PATH, which contain the same command,
-    # appear before their own PATH, thus leading to conflict (e.g.:
-    # `jenv` run the java command located in `$JENV_ROOT/shims/java`,
-    # however, `/usr/local/bin` also contains java).
-    #
-    # This conflit is because I export the PATH in the `zshenv` file,
-    # which sourced before the `/etc/zprofile`. And the `/etc/zprofile`
-    # will call the path_helper command to add the PATH defined in
-    # `/etc/paths` and `/etc/paths.d/*`
-    #
-    # https://stackoverflow.com/questions/26520584/why-is-my-path-much-longer-than-bash-profile
-    # https://stackoverflow.com/questions/9832770/where-is-the-default-terminal-path-located-on-mac
-    # https://github.com/rbenv/rbenv/wiki/Unix-shell-initialization
-
-    if [[ -s /etc/paths ]]; then
-        execute \
-            "sudo mv /etc/paths /etc/paths.bak \
-                  && printf '' | sudo tee -a /etc/paths" \
-            "Diable path_helper (only macOS)"
-    fi
-
-}
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 main() {
 
     brew_install "Zsh" "zsh" \
-        && change_default_shell \
-        && disable_path_helper
+        && change_default_shell
 
 }
 
