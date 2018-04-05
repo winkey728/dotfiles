8c7742ec9efac72ffb9003b455ad90d7358c502e
winkey <winkeyme@163.com>
2018-04-05 21:46:27 +0800

    [Mac -> ide] Add VSCode and Typora

--- src/installations/macos/ide.sh
+++ src/installations/macos/ide.sh
@@ -10,7 +10,16 @@ main() {
 
     print_in_purple "\n   IDEs\n\n"
 
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
     brew_install "Jetbrains Toolbox" "jetbrains-toolbox" "caskroom/cask" "cask"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Editors
+
+    brew_install "Visual Studio Code" "visual-studio-code" "caskroom/cask" "cask"
+    brew_install "Typora" "typora" "caskroom/cask" "cask"
 
 }
 
