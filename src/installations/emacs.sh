802c72964e4692a72f66ed9f6b11cffbbd175937
winkey <winkeyme@163.com>
2018-04-06 21:39:39 +0800

    [All -> emacs] Make symlink to my spacemacs config instead directly clone

--- src/installations/emacs.sh
+++ src/installations/emacs.sh
@@ -10,6 +10,7 @@ declare -r SPACEMACS_GIT_URL="https://github.com/$SPACEMACS_REPO.git"
 
 declare -r MY_CONFIG_REPO="winkey728/spacemacs.d"
 declare -r MY_CONFIG_GIT_URL="https://github.com/$MY_CONFIG_REPO.git"
+declare -r MY_CONFIG_DIRECTORY="$HOME/Projects/github/spacemacs.d"
 
 declare -r EMACS_DOT_D_DIRECTORY="$HOME/.emacs.d"
 declare -r SPACEMACS_DOT_D_DIRECTORY="$HOME/.spacemacs.d"
@@ -56,12 +57,17 @@ setup_spacemacs() {
 
 install_my_config() {
 
-    local dir="$1"
-    local url="$2"
+    local sourceDir="$1"
+    local targetDir="$2"
+    local url="$3"
 
     execute \
-        "git clone --quiet $url $dir" \
-        "My emacs config (install to $dir)"
+        "git clone --quiet $url $sourceDir" \
+        "My emacs config (install to $sourceDir)"
+
+    execute \
+        "ln -fs $sourceDir $targetDir" \
+        "$targetDir → $sourceDir"
 
 }
 
@@ -80,7 +86,10 @@ setup_my_config() {
 
     if [ ! -d "$SPACEMACS_DOT_D_DIRECTORY" ]; then
 
-        install_my_config "$SPACEMACS_DOT_D_DIRECTORY" "$MY_CONFIG_GIT_URL"
+        install_my_config \
+            "$MY_CONFIG_DIRECTORY" \
+            "$SPACEMACS_DOT_D_DIRECTORY" \
+            "$MY_CONFIG_GIT_URL"
 
     else
 
