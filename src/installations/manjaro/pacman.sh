82105a7427db9bd38c1612292c21abcc1a813301
winkey <winkeyme@163.com>
2019-02-21 22:02:54 +0800

    [Manjaro -> pacman] Update pacman mirrorlist to make it faster

--- src/installations/manjaro/pacman.sh
+++ src/installations/manjaro/pacman.sh
@@ -6,9 +6,45 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-main() {
+change_pacman_mirrors() {
 
-    print_in_purple "\n   Pacman\n\n"
+    declare -r PACMAN_MIRRORS_FILE="/etc/pacman.d/mirrorlist"
+    declare -r PACMAN_MIRRORS_BACKUP="/etc/pacman.d/mirrorlist.bak"
+    declare -r CONFIG="\
+##
+## Manjaro Linux default mirrorlist
+##
+
+## Country : China
+Server = https://mirrors.ustc.edu.cn/manjaro/stable/\$repo/\$arch
+
+## Country : China
+Server = https://mirrors.huaweicloud.com/manjaro/stable/\$repo/\$arch
+
+## Country : United States
+Server = https://mirrors.ocf.berkeley.edu/manjaro/stable/\$repo/\$arch
+
+"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Backup original pacman mirrorlist file
+
+    execute \
+        "sudo cp $PACMAN_MIRRORS_FILE $PACMAN_MIRRORS_BACKUP" \
+        "Backup mirrorlist"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Update pacman mirrorlist file
+
+    execute \
+        "printf '%s' '$CONFIG' | sudo tee $PACMAN_MIRRORS_FILE" \
+        "Update mirrorlist"
+
+}
+
+add_package_repositories() {
 
     declare -r PACMAN_CONFIG_FILE="/etc/pacman.conf"
     declare -r CONFIGS="\
@@ -29,6 +65,19 @@ Server = https://cdn.repo.archlinuxcn.org/\$arch
             "printf '%s' '$CONFIGS' | sudo tee -a $PACMAN_CONFIG_FILE" \
             "Pacman (add archlinuxcn repo)"
     fi
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_in_purple "\n   Pacman\n\n"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    change_pacman_mirrors
+    add_package_repositories
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
