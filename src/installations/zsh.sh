b2162a7f01c87bfd10945864b8a73877e549b2e2
黄文海 <huangwh@oupuzw.com>
2019-05-24 12:34:56 +0800

    [All -> zsh] Change theme to powerlevel10k

--- src/installations/zsh.sh
+++ src/installations/zsh.sh
@@ -8,8 +8,8 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 declare -r OMZ_DIRECTORY="$HOME/.oh-my-zsh"
 declare -r OMZ_GIT_REPO_URL="https://github.com/robbyrussell/oh-my-zsh.git"
 
-declare -r POWERLEVEL9K_THEME_DIRECTORY="$OMZ_DIRECTORY/custom/themes/powerlevel9k"
-declare -r POWERLEVEL9K_THEME_GIT_REPO_URL="https://github.com/bhilburn/powerlevel9k.git"
+declare -r POWERLEVEL10K_THEME_DIRECTORY="$OMZ_DIRECTORY/custom/themes/powerlevel10k"
+declare -r POWERLEVEL10K_THEME_GIT_REPO_URL="https://github.com/romkatv/powerlevel10k.git"
 
 declare -r ZSH_AS_DIRECOTRY="$OMZ_DIRECTORY/custom/plugins/zsh-autosuggestions"
 declare -r ZSH_AS_GIT_REPO_URL="https://github.com/zsh-users/zsh-autosuggestions.git"
@@ -34,7 +34,7 @@ update_omz() {
     execute \
         "cd $dir \
             && git pull --quiet --rebase origin master" \
-        "Oh My Zsh (upgrade in $dir)"
+        "Oh My Zsh (update in $dir)"
 
 }
 
@@ -61,7 +61,7 @@ clone_theme() {
 
     execute \
         "git clone --quiet $url $dir" \
-        "powerlevel9k theme (install to $dir)"
+        "powerlevel10k theme (install to $dir)"
 
 }
 
@@ -72,19 +72,19 @@ update_theme() {
     execute \
         "cd $dir \
             && git pull --quiet --rebase origin master" \
-        "powerlevel9k theme (update in $dir)"
+        "powerlevel10k theme (update in $dir)"
 
 }
 
 install_theme() {
 
-    if [ ! -d "$POWERLEVEL9K_THEME_DIRECTORY" ]; then
+    if [ ! -d "$POWERLEVEL10K_THEME_DIRECTORY" ]; then
 
-        clone_theme "$POWERLEVEL9K_THEME_DIRECTORY" "$POWERLEVEL9K_THEME_GIT_REPO_URL"
+        clone_theme "$POWERLEVEL10K_THEME_DIRECTORY" "$POWERLEVEL10K_THEME_GIT_REPO_URL"
 
     else
 
-        update_theme "$POWERLEVEL9K_THEME_DIRECTORY"
+        update_theme "$POWERLEVEL10K_THEME_DIRECTORY"
 
     fi
 
@@ -110,7 +110,7 @@ update_zsh_as() {
     execute \
         "cd $dir \
             && git pull --quiet --rebase origin master" \
-        "zsh-auttosuggestions (upgrade in $dir)"
+        "zsh-auttosuggestions (update in $dir)"
 
 }
 
